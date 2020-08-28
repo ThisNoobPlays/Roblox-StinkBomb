1cd70c51883a1f7d34acb915563cb0589055f3cf
ThisPotatoAimTho <50088553+ThisNoobPlays@users.noreply.github.com>
2020-08-27 18:50:40 -0700

    Add settings module

---
+++ SettingsModule.ModuleScript.lua
@@ -0,0 +1,9 @@
+local bombSettings = {
+  smokeColor = Color3.fromRGB(149, 85, 0), -- Color of smoke and blind GUI. Put in an RGB color code inside the parentheses.
+  maxThrowDistance = 100, -- Maximum distance that the bomb can be thrown. Don't put over 200 because then it can fly across the map.
+  bombColor = BrickColor.new("Dark stone grey"), -- The color of the bomb object. Set to a Roblox color name.
+  bombVelocityMultiplier = 1.5 -- The amount of force used to throw the bomb. Higher number means farther throw. Recommended 1.5 - 2.0
+  
+}
+
+return bombSettings
